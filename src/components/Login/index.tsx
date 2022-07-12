7429146371c49d5695c1b11f1fa02d5ea7520928
Dani <daniverhaar@hotmail.com>
2022-07-12 15:04:48 +0200

    fixed casing

--- src/components/Login/index.tsx
+++
@@ -1,6 +0,0 @@
-import Routes from '../routeConfig';
-
-export default function Login(): any {
-  // simply redirect, nothing else needed.
-  window.location.href = Routes.DiscordLogin;
-}
--- src/components/Login/retry.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Retry(props: any): any {
-  return (
-    <>
-      <h1>Oops... Looks like an unknown error occured. Please try again</h1>
-    </>
-  );
-}
